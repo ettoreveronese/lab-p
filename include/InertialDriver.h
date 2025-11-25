a6ab4c7e475b8e3d997d698015f0cb9b33292e7d
ettoreveronese <everonese50@gmail.com>
2025-11-25 22:54:40 +0000

    update

--- include/InertialDriver.h
+++ include/InertialDriver.h
@@ -1,20 +1,20 @@
-#ifndef INDERTIAL_DRIVER_H
+#ifndef INERTIAL_DRIVER_H
 #define INERTIAL_DRIVER_H
 
 #include "../include/MyVector.h"
 #include <iostream>
 
-struct Reading { // lettura sensore 
+struct Reading { // lettura di 1 sensore 
     double yaw_v, yaw_a, pitch_v, pitch_a, roll_v, roll_a;
 };
     
 const std::size_t NUM_SENSORS = 17;     // numero di sensori, equivale al numero di letture che costituisce una misura
-typedef Reading Measurement[NUM_SENSORS];   // un array di 17 strutture Reading
+typedef Reading Measurement[NUM_SENSORS];   // un array di 17 strutture Reading, quindi le letture di 17 sensori
 
 class InertialDriver{ 
 
 private:
-    const std::size_t BUFFER_DIM = 670;
+    static const std::size_t BUFFER_DIM = 670;
     MyVector<Measurement> buffer;
 
     std::size_t front;
@@ -23,8 +23,8 @@ private:
 
 public:
 	InertialDriver();
-	void push_back(const Measurement& readings);
-	MyVector pop_front();
+	void push_back(const Measurement& new_measurement);
+	void pop_front(Measurement& output_measurement);
 	Reading get_reading(std::size_t i);
 	void clear_buffer();
 	std::ostream& operator<<(std::ostream& os, const InertialDriver& data);
