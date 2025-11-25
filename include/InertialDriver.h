a02c3419f09e7d31d9fbd7a27a90ce1f2d9a6e78
ettoreveronese <everonese50@gmail.com>
2025-11-25 22:03:59 +0000

    update

--- include/InertialDriver.h
+++ include/InertialDriver.h
@@ -4,22 +4,26 @@
 #include "../include/MyVector.h"
 #include <iostream>
 
+struct Reading {
+    double yaw_v, yaw_a, pitch_v, pitch_a, roll_v, roll_a;
+};
+    
+const std::size_t BUFFER_DIM = 670;
+const std::size_t NUM_SENSORS = 17;
+typedef Reading Measurement[NUM_SENSORS];
+
 class InertialDriver{
 
-private:
-    static constexpr std::size_t BUFFER_DIM = 67;
+private: 
     MyVector<Reading> buffer;
 
-    struct Reading {
-        double yaw_v, yaw_a;
-        double pitch_v, pitch_a;
-        double roll_v, roll_a;
-    };
-
+    std::size_t front;
+    std::size_t back;
+    std::size_t count;
 
 public:
 	InertialDriver();
-	void push_back(const Reading measure[BUFFER_DIM]);
+	void push_back(const Measurement& readings);
 	MyVector pop_front();
 	Reading get_reading(std::size_t i);
 	void clear_buffer();
--- src/InertialDriver.cpp
+++ src/InertialDriver.cpp
@@ -2,3 +2,25 @@
 #include <iostream>
 
 InertialDriver::InertialDriver() : buffer(BUFFER_DIM){}
+
+void InertialDriver::push_back(const Measurement& readings){ // todo
+    
+}
+
+MyVector InertialDriver::pop_front(){
+    
+}
+
+Reading InertialDriver::get_reading(){
+
+}
+
+void InertialDriver::clear_buffer(){
+
+}
+
+std::ostream& InertialDriver::operator<<(std::ostream& os, const InertialDriver& data){
+    
+}
+
+
